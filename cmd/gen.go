b0ef9c38a65b0a19880fdd0b1bbc66127dadb9a6
Andy Pan <panjf2000@gmail.com>
2019-04-04 09:18:31 +0800

    Add initial code

---
+++ cmd/gen.go
@@ -0,0 +1,50 @@
+// Copyright © 2019 Andy Pan <panjf2000@gmail.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+
+// genCmd represents the gen command
+var genCmd = &cobra.Command{
+	Use:   "gen",
+	Short: "gen is a 996.icu license generator-command.",
+	Long: `gen is a 996.icu license generator-command,
+it is used to generate various open-source licenses including MIT, Apache, etc.
+More importantly, the main purpose of this tool is to incoporate those aforesaid licenses into
+a brand new license: 996.icu, defined by this repository.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("gen called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(genCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// genCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// genCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.Flags().StringVarP(&list, "list", "l", "", "list all licenses")
+}
---
+++ cmd/root.go
@@ -0,0 +1,58 @@
+// Copyright © 2019 Andy Pan <panjf2000@gmail.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var list string
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "gen-license-go",
+	Short: "gen-license-go is a open-source licenses generator implemented in Go.",
+	Long: `gen-license-go is a 996.icu license generator implemented in Go,
+this generator is developed to generate various open-source licenses including MIT, Apache, etc.
+More importantly, the main purpose of this tool is to incoporate those aforesaid licenses into
+a brand new license: 996.icu, defined by this repository.`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	//	Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.Flags().StringVarP(&list, "list", "l", "", "list all licenses")
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.genLicense.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
